31cb94b3e3f5fbe42f822f4952db5117d76d26a7
Kirill Ushnurtsev <ushnurtsev@gmail.com>
2018-10-13 14:45:51 +0400

    HOL2. Add multitarget

---
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ArrayHelperTests.cs
@@ -0,0 +1,25 @@
+﻿using EPAM.Core.ReportHelper;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPAM.CoreWorkshop.ReportHelper.Tests
+{
+    [TestClass]
+    public class ArrayHelperTests
+    {
+        [TestMethod]
+        public void FillTests()
+        {
+            var array = new int[3];
+
+            ArrayHelper.Fill(array, 5);
+
+            var expectedResult = new int[] { 5, 5, 5 };
+
+            array.Should().BeEquivalentTo(expectedResult);
+        }
+    }
+}
--- src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
+++ src/EPAM.CoreWorkshop.ReportHelper.Tests/ReportNameHelperTests.cs
@@ -1,5 +1,6 @@
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Runtime.InteropServices;
 using System.Threading;
 
 namespace EPAM.Core.ReportHelper.Tests
@@ -7,6 +8,27 @@ namespace EPAM.Core.ReportHelper.Tests
     [TestClass]
     public class ReportNameHelperTests
     {
+        static object[][] NormalizeFileNameTestData
+        {
+            get
+            {
+                object[][] data = new object[][]
+                {
+            new object[]{"Hello: my world", '_', "Hello_ my world"},
+            new object[]{"Hello my %world", '_', "Hello my %world"},
+            new object[]{"Hello my /world", '_', "Hello my _world"}
+                };
+
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    data[0][2] = "Hello: my world";
+                    data[1][2] = "Hello my %world";
+                    data[2][2] = "Hello my _world";
+                }
+                return data;
+            }
+        }
+
         [AssemblyInitialize]
         public static void Init(TestContext context)
         {
@@ -14,9 +36,7 @@ namespace EPAM.Core.ReportHelper.Tests
         }
 
         [TestMethod]
-        //[DataRow("Hello: my world", '_', "Hello_ my world")]
-        [DataRow("Hello my %world", '_', "Hello my %world")]
-        [DataRow("Hello my /world", '_', "Hello my _world")]
+        [DynamicData("NormalizeFileNameTestData", DynamicDataSourceType.Property)]
         public void NormalizeFileNameTest(string name, char repl, string expected)
         {
             var result = ReportNameHelper.NormalizeFileName(name, repl);
